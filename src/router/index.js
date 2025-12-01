3333af22f612b21b0116d87022533191e6c5bbab
azmagainstheworld <zmahdiyyah4@gmail.com>
2025-12-01 17:17:58 +0800

    Change router to hash mode for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '../pages/Home.vue'
 import Products from '../pages/Products.vue'
@@ -8,18 +8,16 @@ import Contact from '../pages/Contact.vue'
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/products', name: 'Products', component: Products },
-
   {
     path: '/products/:id',
     name: 'ProductDetails',
     component: () => import('../pages/ProductDetails.vue')
   },
-
   { path: '/about', name: 'About', component: About },
   { path: '/contact', name: 'Contact', component: Contact }
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),  // <-- ini yang diganti
   routes
 })
